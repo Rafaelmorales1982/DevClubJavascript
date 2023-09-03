2a99b5143fcbeb1af6f37d2c3fd401b9b6f1021b
Rafaelmorales1982 <rafaelmorconcursos@gmail.com>
2023-09-03 19:22:39 -0300

    Commite estudando object

--- 01-object.js
+++ 01-object.js
@@ -1,1 +1,18 @@
-console.log("teste")
+
+const person = {
+    name: "Rafael",
+    age: 41,
+    height: 1.72,
+    sex: "male",
+    address: {
+        street: "Rua josé mantuani",
+        number: 1516,
+        country: "Brasil"
+    }
+}
+
+console.log(person.name, person.age, person.height,
+    person.sex);
+
+console.log(person.address.street, 
+    person.address.number, person.address.country);
